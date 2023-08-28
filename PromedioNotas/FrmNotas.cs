ec136cb80c2bf7497915f0bff749548f94a80dc0
denis <denis@LAPTOP-EOC9IMRR>
2023-08-28 16:56:43 -0600

    Agregar archivos de proyecto.

---
+++ PromedioNotas/FrmNotas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PromedioNotas
+{
+    public partial class FrmNotas : Form
+    {
+        public FrmNotas()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Promedio objPro = new Promedio();
+
+            objPro.Nombre = txtNombre.Text;
+            objPro.Nota1 = int.Parse(txtNota1.Text);
+            objPro.Nota2 = int.Parse(txtNota2.Text);
+            objPro.Nota3 = int.Parse(txtNota3.Text);
+            objPro.Nota4 = int.Parse(txtNota4.Text);
+
+
+            lblNotaBaja.Text = objPro.NotaBaja().ToString("0.00");
+            lblPromedio.Text = objPro.promedio().ToString("0.00");
+            lblCondicion.Text = objPro.condicion();
+
+
+
+
+
+        }
+    }
+}
---
+++ PromedioNotas/Promedio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromedioNotas
+{
+    public class Promedio
+    {
+        // Atributos
+        public string Nombre;
+        public int Nota1;
+        public int Nota2;
+        public int Nota3;
+        public int Nota4;
+            
+           //Metodos
+           
+        public double NotaBaja()
+        {
+            int menor =  int.MaxValue;
+            if (Nota1 < Nota2) menor = Nota1; else menor = Nota2;
+            if (Nota3 < menor) menor = Nota3;
+            if (Nota4 < menor) menor = Nota4;
+            return menor;
+
+        }
+
+        public double promedio()
+        {
+            return (Nota1 + Nota2 + Nota3 + Nota4 - NotaBaja()) / 3.0;
+        }
+
+        public string condicion ()
+        {
+            double prome = promedio();
+            if (prome <= 5 )
+            {
+                return "DESAPROBADO";
+            } else if (prome > 7 && prome <=7.5 )
+            {
+                return "RECUPERACION";
+            }
+            else
+            {
+                return "APROBADO";
+            }
+
+        }
+    }
+}
